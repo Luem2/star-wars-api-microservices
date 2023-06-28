7a08d1577f3ded3c1dba7bbc962ea9f15a10e9e6
Luem <luchemma@gmail.com>
2023-06-28 16:10:32 -0300

    feat: characters middleware & add route

--- characters/src/routers/index.ts
+++ characters/src/routers/index.ts
@@ -1,13 +1,12 @@
 import { Router } from 'https://deno.land/x/oak@v12.5.0/mod.ts'
 import { ctls } from '../controllers/index.ts'
+import { middlewares } from '../middlewares/index.ts'
 
 const router = new Router()
 
 router
     .get('/', ctls.getAllCharacters)
 
-    .get('/characters', (ctx) => {
-        ctx.response.body = 'Hello World'
-    })
+    .post('/', middlewares.characterValidation, ctls.createCharacter)
 
 export default router
