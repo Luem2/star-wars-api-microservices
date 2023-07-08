3d95443aec9dcab14dfe31578fe64a98407b455e
Luem <luchemma@gmail.com>
2023-07-08 02:59:06 -0300

    feat: character model

--- database/src/models/character.model.ts
+++ database/src/models/character.model.ts
@@ -1,33 +1,66 @@
-import { Schema, model } from 'npm:mongoose'
+import { modelOptions, prop, getModelForClass } from 'npm:@typegoose/typegoose'
+import { Model, Types } from 'npm:mongoose'
 
-enum Gender {
-    male = 'male',
-    female = 'female',
+@modelOptions({
+    schemaOptions: { timestamps: true },
+    options: { customName: 'Character' },
+})
+class Character extends Model {
+    @prop({
+        _id: true,
+        default: new Types.ObjectId(),
+    })
+    _id!: string
+
+    @prop({ required: true })
+    name!: string
+
+    @prop({ required: true })
+    height!: string
+
+    @prop({ required: true })
+    mass!: string
+
+    @prop({ required: true })
+    hair_color!: string
+
+    @prop({ required: true })
+    skin_color!: string
+
+    @prop({ required: true })
+    eye_color!: string
+
+    @prop({ required: true })
+    birth_year!: string
+
+    @prop({ required: true })
+    gender!: string
+
+    @prop({ type: String, ref: 'Planet' })
+    homeworld!: string
+
+    @prop({ type: [String], ref: 'Film' })
+    films!: string[]
+
+    public static async list() {
+        return await this.find()
+            .populate('homeworld', ['_id', 'name'])
+            .populate('films', ['_id', 'title'])
+    }
+
+    public static async get(id: string) {
+        return await this.findById(id)
+            .populate('homeworld', ['_id', 'name'])
+            .populate('films', ['_id', 'title'])
+    }
+
+    public static async insert(character: Character) {
+        return await this.create(character)
+    }
+
+    public static async delete(id: string) {
+        return await this.findByIdAndDelete(id)
+    }
 }
 
-const characterSchema = new Schema(
-    {
-        _id: String,
-        name: String,
-        height: String,
-        mass: String,
-        hair_color: String,
-        skin_color: String,
-        eye_color: String,
-        birth_year: String,
-        gender: {
-            type: String,
-            enum: Gender,
-        },
-        homeworld: {
-            type: String,
-            ref: 'Planet',
-        },
-        films: [{ type: String, ref: 'Film' }],
-    },
-    {
-        timestamps: true,
-    }
-)
-
-export const Character = model('Character', characterSchema)
+export const CharacterModel = getModelForClass(Character)
