6de46c6fe64019e36f1297ff32eb18287a14d1e6
Luem <luchemma@gmail.com>
2023-07-08 02:59:26 -0300

    feat: film model

--- database/src/models/film.model.ts
+++ database/src/models/film.model.ts
@@ -1,19 +1,57 @@
-import { Schema, model } from 'npm:mongoose'
+import { modelOptions, prop, getModelForClass } from 'npm:@typegoose/typegoose'
+import { Model, Types } from 'npm:mongoose'
 
-const filmSchema = new Schema(
-    {
-        _id: String,
-        title: String,
-        opening_crawl: String,
-        director: String,
-        producer: String,
-        release_date: Date,
-        characters: [{ type: String, ref: 'Character' }],
-        planets: [{ type: String, ref: 'Planet' }],
-    },
-    {
-        timestamps: true,
+@modelOptions({
+    schemaOptions: { timestamps: true },
+    options: { customName: 'Film' },
+})
+class Film extends Model {
+    @prop({
+        _id: true,
+        default: new Types.ObjectId(),
+    })
+    _id!: string
+
+    @prop({ required: true })
+    title!: string
+
+    @prop({ required: true })
+    opening_crawl!: string
+
+    @prop({ required: true })
+    director!: string
+
+    @prop({ required: true })
+    producer!: string
+
+    @prop({ required: true })
+    release_date!: Date
+
+    @prop({ type: [String], ref: 'Character' })
+    characters!: string[]
+
+    @prop({ type: [String], ref: 'Planet' })
+    planets!: string[]
+
+    public static async list() {
+        return await this.find()
+            .populate('characters', ['_id', 'name'])
+            .populate('planets', ['_id', 'name'])
     }
-)
 
-export const Film = model('Film', filmSchema)
+    public static async get(id: string) {
+        return await this.findById(id)
+            .populate('characters', ['_id', 'name'])
+            .populate('planets', ['_id', 'name'])
+    }
+
+    public static async insert(film: Film) {
+        return await this.create(film)
+    }
+
+    public static async delete(id: string) {
+        return await this.findByIdAndDelete(id)
+    }
+}
+
+export const FilmModel = getModelForClass(Film)
